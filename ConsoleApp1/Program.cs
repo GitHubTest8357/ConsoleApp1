67ad0c11d469bae9036c7c7557af93b3e811896b
SullivP <SullivP@wsdot.wa.gov>
2018-03-09 14:56:50 -0800

    Commit 1st .Net Framework Console App

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+                     
+                Console.WriteLine("Starting Main - Getting Directories");
+
+                string[] myfiles = System.IO.Directory.GetFiles("c:\\users\\sullivp\\desktop");
+
+                Console.WriteLine(myfiles[0]);
+                System.Threading.Thread.Sleep(5000);
+
+                Console.WriteLine("Ending...");
+
+     
         }
     }
 }
